4844291cb9aec983d8e57bef3edee654f469de8d
unknown <C:\Users\User\AppData\Roaming\The Bat!>
2020-02-24 20:12:14 +0300

    docs: update count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,12 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
+module.exports = function countCats(array) { 
+  let number=0;
+  for (let i=0;i<array.length;i++){
+  for (let j=0;j<array[i].length;j++)
+  {
+    if (array[i][j]=="^^") {number++;}
+  }
+}
+  return number;
   // remove line with error and write your code here
 };
+
