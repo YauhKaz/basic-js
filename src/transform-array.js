aa5cd4adf7f85e00026522ab3a0789eab20c7085
unknown <C:\Users\User\AppData\Roaming\The Bat!>
2020-03-18 15:08:06 +0300

    docs: update transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,19 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) throw new Error()
+    else
+    {
+      let buf=[];
+        for (let i=0; i<arr.length;i++)
+        {
+          switch(arr[i])
+          {
+            case '--discard-next':break;
+            case '--discard-prev': { if (i>0) buf.pop();break; }
+            case '--double-next': {if (arr.length-1>i) buf.push(arr[i+1]);break;}
+            case '--double-prev': { if (i>0) buf.push(arr[i-1]);break;}
+            default: {if (arr[i-1]!='--discard-next') buf.push(arr[i]);break;}
+          }
+        }
+        return buf;
+    }    
 };
