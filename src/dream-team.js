5ebd6d4caa1a5c0960b10869cb0589d1ae70bd1b
unknown <C:\Users\User\AppData\Roaming\The Bat!>
2020-03-18 11:27:33 +0300

    docs: update dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let result='';
+  if (typeof(members) != "object" || members==null ) return false;
+  else {
+    for (let i=0;i<members.length;i++){
+      if (typeof(members[i])!='string') continue;
+    for (let j=0;j<members[i].length;j++)
+    if (members[i][j]!=' ') {result+=members[i][j];break;}
+    }
+    return result.toUpperCase().split('').sort().join('');
+  }
 };
