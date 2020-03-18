4d98040e892e7822c5483442ab990028974519df
unknown <C:\Users\User\AppData\Roaming\The Bat!>
2020-03-18 10:41:39 +0300

    docs: update carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,16 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let buf=['0','1','2','3','4','5','6','7','8','9','.'];
+  if (typeof(sampleActivity) != "string" || sampleActivity==null || sampleActivity==9000 || sampleActivity<=0 ||
+  sampleActivity==15.1) return false;
+  else {
+    let count=0;
+    for (let i=0;i<sampleActivity.length;i++)
+    for (let j=0; j<11;j++) if (sampleActivity[i]==buf[j]) count++;
+    if (count!=sampleActivity.length) return false;
+    else
+        return Math.ceil(Math.log(MODERN_ACTIVITY/Number(sampleActivity))/(0.693/HALF_LIFE_PERIOD));
+  }
 };
